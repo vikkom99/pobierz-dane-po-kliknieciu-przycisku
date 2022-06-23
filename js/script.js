2c91c514460d1a84a9660cbbebb7b9223db93fec
vikkom99 <wikkomarik@gmail.com>
2022-06-23 03:59:07 +0100

    pobierz-dane-po-kliknieciu-przycisku

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let btn = document.getElementById('download');
+btn.addEventListener("click", getData);
+
+function getData() {
+
+    fetch(' https://akademia108.pl/api/ajax/get-post.php', {
+        method: 'GET',
+    })
+        .then(response => response.json())
+        .then((data) => {
+
+            let pId = document.createElement('p');
+            let pUserId = document.createElement('p');
+            let pTitle = document.createElement('p');
+            let pBody = document.createElement('p');
+            let hr = document.createElement('hr')
+
+            pId.innerText =`ID: ${data.id}`;
+            pUserId.innerText =`User ID: ${data.userId}`;
+            pTitle.innerText =`Title: ${data.title}`;
+            pBody.innerText =`Body: ${data.body}`;
+
+            document.body.appendChild(pId);
+            document.body.appendChild(pUserId);
+            document.body.appendChild(pTitle);
+            document.body.appendChild(pBody);
+            document.body.appendChild(hr);
+
+        })
+
+}
+
+
+
